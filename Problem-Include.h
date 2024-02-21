36e281289c363904adeef17e18fb038c6b3e87cc
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2024-02-21 10:29:24 +0500

    Add files via upload

---
+++ Problem-Include.h
@@ -0,0 +1,15 @@
+/*==============================================================================
+Project: LiFe
+Theme: Edge movement method (MPI)
+Module: Problem-Include.h ("Includ" Files of the Problem)
+Authors: Nikolay A. Olkhovsky & Leonid B. Sokolinsky
+This source code has been produced with using BSF-skeleton
+==============================================================================*/
+#pragma once
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <string>
