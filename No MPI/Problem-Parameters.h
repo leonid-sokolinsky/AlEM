bd5f7c5bed59c50fcc5d3c75838ee55f3a9685b9
Leonid Sokolinsky <29542960+leonid-sokolinsky@users.noreply.github.com>
2024-07-25 11:00:03 +0500

    Add files via upload
    Minor changes.

--- No MPI/Problem-Parameters.h
+++ No MPI/Problem-Parameters.h
@@ -11,7 +11,7 @@ This source code has been produced with using BSF-skeleton
 
 //#define PP_PATH "Problems/"
 
-/**/
+/**
 #include "../_Problems05-1.h"
 #define PP_PATH "D:/YandexDisk/_private/Programming/Set-of-LP-Problems/Rnd-LP/"
 
@@ -35,7 +35,7 @@ This source code has been produced with using BSF-skeleton
 #include "../_Problems-Klee-Minty-Cube.h"
 #define PP_PATH "D:/YandexDisk/_private/Programming/Set-of-LP-Problems/Klee-Minty-Cube/"
 
-/**
+/**/
 #include "../_Problems-Miscellaneous.h"
 #define PP_PATH "D:/YandexDisk/_private/Programming/Set-of-LP-Problems/Miscellaneous-LP/"
 
